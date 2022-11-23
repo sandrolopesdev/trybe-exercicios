6c4b84ff78e08eabf4a3ee1fbcdc7ef8564390a3
sandrolopesdev <sandrolopesfilho@gmail.com>
2022-11-23 20:51:02 -0300

    Resolução exercício 2, dia 4.1

--- Módulo-Fundamentos/Seção-4/dia-4.1-javasript-primeirospassos/script.js
+++ Módulo-Fundamentos/Seção-4/dia-4.1-javasript-primeirospassos/script.js
@@ -10,3 +10,20 @@ if (a > b) {
 else if (b > a) {
   console.log(`O maior número é: ${b}`); //Utilização de template literals
 }
+
+//Exercício 2:
+//Variáveis que armazenam os valores a serem comparados
+const a = 2;
+const b = 2;
+const c = 2;
+
+//Lógica que irá comparar 3 valores e retrnar o mais deles
+if (a > b && a > c) {
+  console.log(`O número ${a} é o maior deles!`); //Utilização de template literals
+} else if (b > a && b > c) {
+  console.log(`O número ${b} é o maior deles!`); //Utilização de template literals
+} else if (c > a && c > b) {
+  console.log(`O número ${c} é o maior deles!`); //Utilização de template literals
+} else {
+  console.log(`Os valores são iguais`); //Utilização de template literals
+}
