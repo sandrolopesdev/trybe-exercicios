31b68405e068233d4ae5f9ab70366e71c568995c
sandrolopesdev <sandrolopesfilho@gmail.com>
2022-11-30 21:32:42 -0300

    finalizado exercícios padrão

--- Módulo-Fundamentos/Seção-4/dia-4.4-javascript-objetos-funcoes/script.js
+++ Módulo-Fundamentos/Seção-4/dia-4.4-javascript-objetos-funcoes/script.js
@@ -106,3 +106,45 @@ info.recorrente = 'Sim';
 console.log(info);
 
 // Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+
+console.log(`RETORANDO SOMENTE AS CHAVES:`);
+const iteraInfoKey = () => {
+  for (let key in info) {
+    console.log(key); // Retornando apenas as chaves
+  }
+};
+iteraInfoKey();
+
+// Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+
+console.log(`RETORNANDO OS VALORES CONTIDOS NAS CHAVES:`);
+const iteraInfoKeyValue = () => {
+  for (let key in info) {
+    console.log(info[key]); // Retornando somente os valores contidos nas chaves
+  }
+};
+iteraInfoKeyValue();
+
+// Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, 
+// Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. Valor esperado no console:
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+}
+
+const concat = () => {
+  for (let keyInfo in info) {
+    console.log(`${info[keyInfo]} e ${info2[keyInfo]}`);
+  }
+};
+concat();
+
